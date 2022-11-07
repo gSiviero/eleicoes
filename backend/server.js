3ee5ec68f6d3d4f5f4222177c4e36689a63fd007
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2022-11-07 12:39:59 -0800

    Last Ajustments

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/",(req,res) => res.send("OK"));
-app.post("/sessoes", (req, res) => {
+app.use("/",express.static("../frontend/build"))
+app.post("/secoes", (req, res) => {
   getSecoes(req.body).then((a) => {
     // console.log(a);
     res.send({data:a[0],numberOfRows:a[1]})});
